3c737227d96a8fc1904372fecb6c3fe5cf1d7278
hldup <therealkispetofi@gmail.com>
2023-11-23 15:27:57 +0100

    oops

--- forum-service/src/main/java/hu/szakkor/forum/ForumController.java
+++ forum-service/src/main/java/hu/szakkor/forum/ForumController.java
@@ -1,14 +1,12 @@
 package hu.szakkor.forum;
 
-<<<<<<< HEAD
-=======
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
->>>>>>> db40d4dbb20128609bacb60fd597903df19df2c2
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,14 +15,13 @@ public class ForumController {
     private final ForumService forumService;
 
     @GetMapping
-    public ResponseEntity<List<Forum>> findAll(){
+    public ResponseEntity<List<Forum>> findAll() {
         return ResponseEntity.ok(forumService.findAll());
     }
 
-<<<<<<< HEAD
     @GetMapping
-    public ResponseEntity<List<Forum>> findForum() {
-        return ResponseEntity.ok(forumService.findForum());
+    public ResponseEntity<List<Forum>> FindAll() {
+        return ResponseEntity.ok(forumService.findAll());
     }
 
     @PostMapping
@@ -34,37 +31,17 @@ public class ForumController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateForum(@PathVariable String uuid) {
-=======
-    @GetMapping("/{id}")
-    public ResponseEntity<Forum> getForumById(@PathVariable UUID uuid) {
-        return ResponseEntity.ok(forumService.findByID(uuid));
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> createNewForum(@RequestBody Forum forum){
-        forumService.createForum(forum);
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateForum(@PathVariable Forum forum){
-        forumService.updateForum(forum);
->>>>>>> db40d4dbb20128609bacb60fd597903df19df2c2
         return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-<<<<<<< HEAD
     public ResponseEntity<Void> deleteForum(@PathVariable String uuid) {
         return ResponseEntity.notFound().build();
     }
 
-=======
-    public ResponseEntity<Void> deleteForum(@PathVariable UUID uuid){
+    public ResponseEntity<Void> deleteForum(@PathVariable UUID uuid) {
         // TODO: 11/9/2023 finish this
         return ResponseEntity.notFound().build();
     }
 
-
->>>>>>> db40d4dbb20128609bacb60fd597903df19df2c2
 }
