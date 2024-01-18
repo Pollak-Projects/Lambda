80e4ad29b83289bfd82133f149f5b5b3c4365042
TGyAkos <t.gyula.akos@gmail.com>
2024-01-18 15:06:28 +0100

    added chackra ui

--- lambda-client/src/pages/Settings.tsx
+++ lambda-client/src/pages/Settings.tsx
@@ -1,9 +1,10 @@
-const Settings = () => {
+// import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
+// import { MdStar } from "react-icons/md";
+// TODO finish xd
+export default function Settings () {
     return (
         <>
-            <h1>Settings</h1>
+
         </>
     );
 }
-
-export default Settings;
