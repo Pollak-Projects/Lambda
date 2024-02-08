9cd7b9e576018009a57c6e16ec503d565fae0f6b
Seiwy <bmate20050911@gmail.com>
2024-02-08 16:56:45 +0100

    There are boxes, Yippeee!

--- lambda-client/src/pages/ForumGridPage.tsx
+++ lambda-client/src/pages/ForumGridPage.tsx
@@ -1,20 +1,25 @@
 import "./ForumGridPage.css"
-import {  Grid, GridItem } from '@chakra-ui/react'
+import {Grid, GridItem, SimpleGrid, Box, Link, List, ListItem, UnorderedList} from '@chakra-ui/react'
+import {NavLink} from "react-router-dom";
+import Navbar from "../components/Navbar.tsx";
+
 function ForumGridPage(){
 
-    function generateForumGridElements(){
+    /*function generateForumGridElements(){
         for(let i = 0; i < 9; i++){
-          let gridContent = document.createElement("div")
-         /* gridContent.id = String(i)
-          gridContent.innerHTML = "idk bro"
-          gridContent.setAttribute("class", "gridElements")
-        //@ts-ignore
-          document.getElementById("forumGridContent").appendChild(gridContent) make it in html */
-    
+            const gridContent = document.createElement("div")
+            gridContent.id = String(i)
+            gridContent.innerHTML = "idk bro"
+            gridContent.setAttribute("class", "gridElements")
+            //@ts-expect-error possible null
+            document.getElementById("forumGridContent").appendChild(gridContent)
+
+
         }
-      }
+    }*/
 
-
+    // @ts-ignore
+    // @ts-ignore
     return(
         <>
 
@@ -30,16 +35,38 @@ function ForumGridPage(){
             fontWeight='bold'
             >
             <GridItem pl='2' bg='orange.300' area={'header'}>
-                Header
+//fx this shit ty
+                <Navbar>
+                    <UnorderedList className="leftNav">
+                        <ListItem><Link as={NavLink} to="/home">Home</Link></ListItem>
+                    </UnorderedList>
+                </Navbar>
+                <nav >
+                    <ul className="rightNav">
+                        <li><a>Notifications</a></li>
+                        <li><a>Profile</a></li>
+                        <li><a> Settings</a></li>
+                    </ul>
+                </nav>
             </GridItem>
             <GridItem pl='2' bg='pink.300' area={'nav'}>
                 Nav
             </GridItem>
-            <GridItem pl='2' bg='green.300' area={'main'}>
+            <GridItem  bg='green.300' area={'main'}>
 
 
-                Main
-                <div className="column_middle" id="forumGridContent"></div>
+
+                <div className="column_middle" id="forumGridContent" >
+                    <SimpleGrid minChildWidth='30em' spacing='10px' >
+                        <Box bg='tomato'  className="gridElements"></Box>
+                        <Box bg='tomato'  className="gridElements"></Box>
+                        <Box bg='tomato'  className="gridElements"></Box>
+                        <Box bg='tomato'  className="gridElements"></Box>
+                        <Box bg='tomato'  className="gridElements"></Box>
+                        <Box bg='tomato'  className="gridElements"></Box>
+                    </SimpleGrid>
+
+                </div>
 
 
 
